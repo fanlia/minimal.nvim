12d38dd1fa713e80133fcf961f956cd8a24481bd
fanlia <you@example.com>
2023-02-25 17:29:49 +0800

    first commit

---
+++ init.lua
@@ -0,0 +1,35 @@
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "git@github.com:folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- example using a list of specs with the default options
+vim.g.mapleader = " " -- make sure to set `mapleader` before lazy so your mappings are correct
+
+require("lazy").setup({
+    { 'nvim-telescope/telescope.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
+    'nvim-tree/nvim-tree.lua',
+}, {
+    git = { url_format = 'git@github.com:%s.git' },
+})
+
+-- nvim-tree
+require("nvim-tree").setup()
+vim.keymap.set("n", "<leader>nn", ":NvimTreeToggle<CR>", {})
+
+-- telescope
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
