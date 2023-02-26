d38593ead71a5f25d0c098caabee8cf909133401
fanlia <you@example.com>
2023-02-26 23:53:43 +0800

    first commit

--- init.lua
+++ init.lua
@@ -12,10 +12,16 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = " " 				-- make sure to set `mapleader` before lazy so your mappings are correct
+vim.g.mapleader = " "                           -- make sure to set `mapleader` before lazy so your mappings are correct
 vim.opt.expandtab = true                        -- convert tabs to spaces
 vim.opt.shiftwidth = 2                          -- the number of spaces inserted for each indentation
 vim.opt.tabstop = 2                             -- insert 2 spaces for a tab
+vim.opt.number = true                           -- line number
+vim.opt.relativenumber = true                   -- relative line number
+vim.opt.cursorline = true                       -- highlight current line
+vim.opt.undofile = true                         -- undo file
+vim.opt.undolevels = 100                        -- undo file levels
+vim.opt.list = true                             -- show strange char
 
 require("lazy").setup({
   { 'nvim-telescope/telescope.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
