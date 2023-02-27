4f25d4edd714d5a01f8173ddbf074e29d7ab43ab
fanlia <you@example.com>
2023-02-27 10:02:51 +0800

    first commit

--- init.lua
+++ init.lua
@@ -45,6 +45,7 @@ vim.keymap.set("n", "<leader>nn", ":NvimTreeToggle<CR>", {})
 -- nvim-telescope/telescope.nvim
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 
 -- numToStr/Comment.nvim
