171b7512832d85b482facdc2aa382fa6c6a9423d
uxnow <odootest2023@163.com>
2024-05-21 16:09:53 +0800

    remove numToStr/Comment.nvim since nvim 0.10 builtin

--- init.lua
+++ init.lua
@@ -30,7 +30,6 @@ vim.opt.list = true                             -- show strange char
 local plugins = {
   { 'nvim-telescope/telescope.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
   'nvim-tree/nvim-tree.lua',
-  'numToStr/Comment.nvim',
   'folke/tokyonight.nvim',
   'windwp/nvim-autopairs',
   'lukas-reineke/indent-blankline.nvim',
@@ -65,9 +64,6 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-
--- numToStr/Comment.nvim
-require('Comment').setup()
 
 -- windwp/nvim-autopairs
 require('nvim-autopairs').setup()
